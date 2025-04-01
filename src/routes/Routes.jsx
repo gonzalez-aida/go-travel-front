3560bc1f587af4e46857037af418198894d3a472
gonzalez-aida <aidalu13oct@gmail.com>
2025-03-31 19:37:23 -0600

    hashRouter

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "../Pages/Login/LoginPage";
 import HomePage from "../Pages/Home/HomePage";
 import RegisterPage from "../Pages/Register/RegisterPage";
@@ -11,37 +11,39 @@ import ProtectedRoute from "./ProtectedRoute";
 
 const AppRoutes = () => {
     return (
-        <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/home" element={<HomePage />} />
+        <Router>
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/home" element={<HomePage />} />
 
-            <Route path="/profile" element={
-                <ProtectedRoute>
-                    <ProfilePage />
-                </ProtectedRoute>
-            } />
-            <Route path="/flights" element={
-                <ProtectedRoute>
-                    <FlightPage />
-                </ProtectedRoute>
-            } />
-            <Route path="/hotels" element={
-                <ProtectedRoute>
-                    <HotelPage />
-                </ProtectedRoute>
-            } />
-            <Route path="/contact" element={
-                <ProtectedRoute>
-                    <ContactPage />
-                </ProtectedRoute>
-            } />
-            <Route path="/offerts" element={
-                <ProtectedRoute>
-                    <OffersPage />
-                </ProtectedRoute>
-            } />
-        </Routes>
+                <Route path="/profile" element={
+                    <ProtectedRoute>
+                        <ProfilePage />
+                    </ProtectedRoute>
+                } />
+                <Route path="/flights" element={
+                    <ProtectedRoute>
+                        <FlightPage />
+                    </ProtectedRoute>
+                } />
+                <Route path="/hotels" element={
+                    <ProtectedRoute>
+                        <HotelPage />
+                    </ProtectedRoute>
+                } />
+                <Route path="/contact" element={
+                    <ProtectedRoute>
+                        <ContactPage />
+                    </ProtectedRoute>
+                } />
+                <Route path="/offerts" element={
+                    <ProtectedRoute>
+                        <OffersPage />
+                    </ProtectedRoute>
+                } />
+            </Routes>
+        </Router>
     );
 };
 
